05230e3c7da5720bdd724d46dd602b4f77c27537
Unknown <sirhans@gmail.com>
2019-07-05 18:03:29 +0700

    update BMWaveshaping

--- AudioFilters/NonlinearGain/BMWaveshaping.c
+++ AudioFilters/NonlinearGain/BMWaveshaping.c
@@ -6,50 +6,115 @@
 //  Anyone may use this file without restrictions of any kind
 //
 
-#import <Accelerate/Accelerate.h>
+#include <Accelerate/Accelerate.h>
+#include <simd/simd.h>
 
 /*!
  *BMWaveshaper_processBufferBidirectional
  * @abstract clips the input to [-1,1] with a cubic soft clipping function
  */
-void BMWaveshaper_processBufferBidirectional(const float* input, float* output, float* tempBuffer, size_t numSamples){
+void BMWaveshaper_processBufferBidirectional(const float* input, float* output, size_t numSamples){
     // Waveshaping function on [-1,1] with continuous derivatives at the boundaries
     // Mathematica:
     //   waveshaping[x_]:=-(1/2)Clip[x]^3 + (3/2)Clip[x]
+    //
+    // hard clip to [-1,1]
     float one = 1.0f;
     float negOne = -1.0f;
-    
-    // hard clip to [-1,1]
-    vDSP_vclip(input, 1, &negOne, &one, tempBuffer, 1, numSamples);
-    
+    vDSP_vclip(input, 1, &negOne, &one, output, 1, numSamples);
+    //
+    //
     // use third order polynomial to apply waveshaping
     // -(1/2)Clip[x]^3 + (3/2)Clip[x] = (x/2)(3 - x^2)
-    float* x = tempBuffer;
-    vDSP_vsq(x, 1, output, 1, numSamples); // x^2 in output
-    float neg3 = -3.0f;
-    vDSP_vsadd(output, 1, &neg3, output, 1, numSamples); // (x^2 - 3)
-    vDSP_vmul(x,1,output,1,output,1,numSamples); // x (x^2 - 3)
-    float negHalf = -0.5f;
-    vDSP_vsmul(output, 1, &negHalf, output, 1, numSamples); // (x/2)(3 - x^2)
+//    float* x = tempBuffer;
+//    vDSP_vsq(x, 1, output, 1, numSamples); // x^2 in output
+//    float neg3 = -3.0f;
+//    vDSP_vsadd(output, 1, &neg3, output, 1, numSamples); // (x^2 - 3)
+//    vDSP_vmul(x,1,output,1,output,1,numSamples); // x (x^2 - 3)
+//    float negHalf = -0.5f;
+//    vDSP_vsmul(output, 1, &negHalf, output, 1, numSamples); // (x/2)(3 - x^2)
+    
+    // apply the polynomial function -(1/2)Clip[x]^3 + (3/2)Clip[x]
+    //float coefficients [4] = {-0.5f,0.0f,1.5f,0.0f};
+    //vDSP_vpoly(coefficients, 1, output, 1, output, 1, numSamples, 3);
+    
+    // apply the polynomial function -(1/2) x^3 + (3/2) x
+    while(numSamples >= 4){
+        simd_float4* x = (simd_float4*)output;
+        *x = *x * (*x * *x - 3.0f) * -0.5f;
+        output+=4;
+    }
+    // finish up the last n <= 3 samples if necessary
+    while (numSamples > 0){
+        *output = *output * (*output + *output - 3.0f) * -0.5f;
+        output++;
+    }
 }
 
 
 
 /*!
  *BMWaveshaper_processBufferPositive
- * @abstract clips the input to [-inf,1] with a cubic soft clipping function
- * @input    an array of values in the range [0,inf]. Negative values are ok but the waveshaping will distort them in a way that may not be useful.
+ * @abstract clips the input to [0,1] with a cubic soft clipping function
+ * @input    an array of values in the range [0,1].
  * @output   if the input is in [0,inf] then the output will be in [0,1]
  */
 void BMWaveshaper_processBufferPositive(const float* input, float* output, size_t numSamples){
+    // Mathematica:
+    //     waveshapingPos[x_] := 2 Clip[x] - Clip[x]^2
     
+    // clip values to
+    float zero = 0.0f;
+    float negOne = 1.0f;
+    vDSP_vclip(input, 1, &zero, &negOne, output, 1, numSamples);
+    
+    // apply the polynomial function 2 x - x^2
+    //float coefficients [3] = {-1.0f,2.0f,0.0f};
+    //vDSP_vpoly(coefficients, 1, output, 1, output, 1, numSamples, 2);
+    
+    // apply the polynomial function 2 x - x^2
+    simd_float4 two = 2.0f;
+    while(numSamples >= 4){
+        simd_float4* x = (simd_float4*)output;
+        *x = *x * (two - *x);
+        output+=4;
+    }
+    // finish up the last n <= 3 samples if necessary
+    while (numSamples > 0){
+        *output = *output * (2.0f - *output);
+        output++;
+    }
 }
 
 
 
 //
 void BMWaveshaper_processBufferNegative(const float* input, float* output, size_t numSamples){
+    // Mathematica:
+    //     waveshapingPos[x_] := 2 Clip[x] + Clip[x]^2
     
+    // clip values to [-1,0]
+    float zero = 0.0f;
+    float negOne = -1.0f;
+    vDSP_vclip(input, 1, &negOne, &zero, output, 1, numSamples);
+    
+    // apply the polynomial function 2 x + x^2
+    //float coefficients [3] = {1.0f,2.0f,0.0f};
+    //vDSP_vpoly(coefficients, 1, output, 1, output, 1, numSamples, 2);
+    
+    // apply the polynomial function 2 x + x^2
+    // in chunks of four samples
+    simd_float4 two = 2.0f;
+    while(numSamples >= 4){
+        simd_float4* x = (simd_float4*)output;
+        *x = *x * (two + *x);
+        output+=4;
+    }
+    // finish up the last n <= 3 samples if necessary
+    while (numSamples > 0){
+        *output = *output * (2.0f - *output);
+        output++;
+    }
 }
 
 
--- AudioFilters/NonlinearGain/BMWaveshaping.h
+++ AudioFilters/NonlinearGain/BMWaveshaping.h
@@ -23,8 +23,8 @@ void BMWaveshaper_processBufferBidirectional(const float* input, float* output, 
 
 /*!
  *BMWaveshaper_processBufferPositive
- * @abstract clips the input to [-inf,1] with a cubic soft clipping function
- * @input    an array of values in the range [0,inf]. Negative values are ok but the waveshaping will distort them in a way that may not be useful.
+ * @abstract clips the input to [0,1] with a cubic soft clipping function
+ * @input    an array of values in the range [0,inf].
  * @output   if the input is in [0,inf] then the output will be in [0,1]
  */
 void BMWaveshaper_processBufferPositive(const float* input, float* output, size_t numSamples);
@@ -33,8 +33,8 @@ void BMWaveshaper_processBufferPositive(const float* input, float* output, size_
 
 /*!
  *BMWaveshaper_processBufferNegative
- * @abstract clips the input to [-1,inf] with a cubic soft clipping function
- * @input    an array of values in the range [-inf,0]. Positive values are ok but the waveshaping will distort them in a way that may not be useful.
+ * @abstract clips the input to [-1,0] with a cubic soft clipping function
+ * @input    an array of values in the range [-1,0].
  * @output   if the input is in [-inf,0] then the output will be in [-1,0]
  */
 void BMWaveshaper_processBufferNegative(const float* input, float* output, size_t numSamples);
