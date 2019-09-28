30748e00a9f4a62f270977e372b451c2510393a0
Unknown <sirhans@gmail.com>
2019-09-28 11:13:01 +0700

    added BMShortSimpleDelay

---
+++ AudioFilters/DelayAndReverb/BMShortSimpleDelay.c
@@ -0,0 +1,9 @@
+//
+//  BMShortSimpleDelay.c
+//  SaturatorAU
+//
+//  Created by hans anderson on 9/27/19.
+//  Copyright © 2019 bluemangoo. All rights reserved.
+//
+
+#include "BMShortSimpleDelay.h"
---
+++ AudioFilters/DelayAndReverb/BMShortSimpleDelay.h
@@ -0,0 +1,14 @@
+//
+//  BMShortSimpleDelay.h
+//  SaturatorAU
+//
+//  Created by hans anderson on 9/27/19.
+//  Copyright © 2019 bluemangoo. All rights reserved.
+//
+
+#ifndef BMShortSimpleDelay_h
+#define BMShortSimpleDelay_h
+
+#include <stdio.h>
+
+#endif /* BMShortSimpleDelay_h */
