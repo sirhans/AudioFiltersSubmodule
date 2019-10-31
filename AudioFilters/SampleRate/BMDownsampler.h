91bf4e9d6c5c46b146ddd8a071783ca9e6993933
Unknown <sirhans@gmail.com>
2019-10-31 15:54:46 +0700

    remove source control markers from downsmampler.h

--- AudioFilters/SampleRate/BMDownsampler.h
+++ AudioFilters/SampleRate/BMDownsampler.h
@@ -17,7 +17,6 @@ extern "C" {
 #include <MacTypes.h>
 #include "BMIIRDownsampler2x.h"
 #include "BMMultiLevelBiquad.h"
-<<<<<<< HEAD
 #include "BMUpsampler.h"
 
 typedef struct BMDownsampler {
@@ -75,65 +74,6 @@ void BMDownsampler_free(BMDownsampler* This);
 void BMDownsampler_impulseResponse(BMDownsampler* This, float* IR, size_t IRLength);
 
 
-=======
-    
-    typedef struct BMDownsampler {
-        BMIIRDownsampler2x* downsamplers2x;
-        BMMultiLevelBiquad antiRingingFilter;
-        float *bufferL1, *bufferR1, *bufferL2, *bufferR2;
-        size_t numStages, downsampleFactor;
-        bool stereo;
-    } BMDownsampler;
-    
-    
-    /*!
-     *BMDownsampler_init
-     * @This   pointer to a BMDownsampler struct
-     * @stereo set true for stereo AA filters; false for mono
-     * @param  downsampleFactor supported values: 2^n
-     */
-    void BMDownsampler_init(BMDownsampler *This, bool stereo, size_t downsampleFactor);
-    
-    
-    
-    /*!
-     *BMDownsampler_processBufferMono
-     *
-     * @absract upsample a buffer that contains a single channel of audio samples
-     *
-     * @param input    length = numSamplesIn
-     * @param output   length = numSamplesIn / upsampleFactor
-     * @param numSamplesIn  number of input samples to process
-     */
-    void BMDownsampler_processBufferMono(BMDownsampler *This, float* input, float* output, size_t numSamplesIn);
-    
-    
-    /*!
-     *BMDownsampler_processBufferStereo
-     *
-     * @absract upsample a buffer that contains a single channel of audio samples
-     *
-     * @param inputL    length = numSamplesIn
-     * @param inputR    length = numSamplesIn
-     * @param outputL   length = numSamplesIn / upsampleFactor
-     * @param outputR   length = numSamplesIn / upsampleFactor
-     * @param numSamplesIn  number of input samples to process
-     */
-    void BMDownsampler_processBufferStereo(BMDownsampler *This, float* inputL, float* inputR, float* outputL, float* outputR, size_t numSamplesIn);
-    
-    
-    void BMDownsampler_free(BMDownsampler *This);
-    
-    
-    /*!
-     *BMDownsampler_impulseResponse
-     * @param IR empty array for storing the impulse response with length = IRLength
-     * @param IRLength -
-     */
-    void BMDownsampler_impulseResponse(BMDownsampler *This, float* IR, size_t IRLength);
-    
-    
->>>>>>> master
 #endif /* BMDownsampler_h */
 
 #ifdef __cplusplus
