0946001f426b046b42654a0d119554ed13b0aee1
Unknown <sirhans@gmail.com>
2019-12-14 10:50:57 +0700

    Starting Huge reverb

---
+++ AudioFilters/DelayAndReverb/BMHugeReverb.c
@@ -0,0 +1,11 @@
+//
+//  BMHugeReverb.c
+//  AudioFiltersXcodeProject
+//
+//  Created by hans anderson on 12/6/19.
+//  Copyright © 2019 BlueMangoo. All rights reserved.
+//
+
+#include "BMHugeReverb.h"
+
+
---
+++ AudioFilters/DelayAndReverb/BMHugeReverb.h
@@ -0,0 +1,18 @@
+//
+//  BMHugeReverb.h
+//  AudioFiltersXcodeProject
+//
+//  Created by hans anderson on 12/6/19.
+//  Copyright © 2019 BlueMangoo. All rights reserved.
+//
+
+#ifndef BMHugeReverb_h
+#define BMHugeReverb_h
+
+#include <stdio.h>
+
+typedef struct BMHugeReverb {
+	BMFDN16 
+} BMHugeReverb;
+
+#endif /* BMHugeReverb_h */
