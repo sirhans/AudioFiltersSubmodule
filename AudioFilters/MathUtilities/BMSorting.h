f1c3d9f653dac9b1024032423565dc0d29988743
Unknown <sirhans@gmail.com>
2019-12-18 16:51:24 +0700

    put insertion sort in BMSorting

---
+++ AudioFilters/MathUtilities/BMSorting.h
@@ -0,0 +1,16 @@
+//
+//  BMSorting.h
+//  SaturatorAU
+//
+//  Created by hans anderson on 12/18/19.
+//  Copyright © 2019 bluemangoo. All rights reserved.
+//
+
+#ifndef BMSorting_h
+#define BMSorting_h
+
+#include <stdio.h>
+
+void BMInsertionSort_size_t(size_t *a, size_t n);
+
+#endif /* BMSorting_h */
