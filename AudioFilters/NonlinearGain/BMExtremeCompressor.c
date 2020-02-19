40f7a1d273871a8de144c89fd84b86d6e7d6fc84
Unknown <sirhans@gmail.com>
2020-02-19 10:31:06 +0700

    begin extreme compressor and attack shaper

---
+++ AudioFilters/NonlinearGain/BMExtremeCompressor.c
@@ -0,0 +1,9 @@
+//
+//  BMExtremeCompressor.c
+//  AudioFiltersXcodeProject
+//
+//  Created by Hans on 18/2/20.
+//  Copyright © 2020 BlueMangoo. All rights reserved.
+//
+
+#include "BMExtremeCompressor.h"
---
+++ AudioFilters/NonlinearGain/BMExtremeCompressor.h
@@ -0,0 +1,14 @@
+//
+//  BMExtremeCompressor.h
+//  AudioFiltersXcodeProject
+//
+//  Created by Hans on 18/2/20.
+//  Copyright © 2020 BlueMangoo. All rights reserved.
+//
+
+#ifndef BMExtremeCompressor_h
+#define BMExtremeCompressor_h
+
+#include <stdio.h>
+
+#endif /* BMExtremeCompressor_h */
