c44cb1fa7ec3e160672f8abea5b374473ac4ffe2
Unknown <sirhans@gmail.com>
2020-08-18 17:33:40 +0700

    make bHSV more balanced

--- AudioFilters/Graphics/BMColourFunctions.c
+++ AudioFilters/Graphics/BMColourFunctions.c
@@ -22,6 +22,40 @@ simd_float3 saturate3(simd_float3 v) {
 const simd_float3 bHSVZeroH = {0.5,3.5/3,2.5/3};
 
 
+float signedSquare(float x){
+	return x * x * simd_sign(x);
+}
+
+
+float signedPow(float x, float p){
+	return powf(fabsf(x), p) * simd_sign(x);
+}
+
+
+
+/*!
+ *evenHueMod
+ *
+ * Scale the hue value so that in a rainbow gradient the width of the R, G, B
+ * appears similar to the width of the Y, C M colours
+ */
+float evenHueMod(float h){
+	/* prototype:
+	 *
+	 * SignedPow[x_,p_]:=Abs[x]^pow * Sign[x]
+	 * p = 1.75;
+	 * ModH[h_]:=(IntegerPart[3h+0.5]+ 2^(p-1)SignedPow[FractionalPart[3h+0.5]-0.5,p])/3
+	 */
+	float scaledH = 3.0f * h + 0.5f;
+	float integerPart = floor(scaledH);
+	float p = 1.75f;
+	float fractionalPart = powf(2.0f, p - 1.0f) * signedPow(simd_fract(scaledH)-0.5f,p);
+	return (integerPart + fractionalPart) / 3.0f;
+}
+
+
+
+
 
 /*!
  *bHSVToRGB
@@ -32,13 +66,14 @@ const simd_float3 bHSVZeroH = {0.5,3.5/3,2.5/3};
  * adjusting the hue.
  */
 simd_float3 bHSVToRGB(float h, float s, float v){
-	simd_float3 bHSV = {h,s,v};
+	
+	simd_float3 bHSV = {evenHueMod(h),s,v};
 	
 	// get the pure hue as an RGB colour
 	simd_float3 hueRGB = saturate3(6.0f * simd_abs(simd_fract(bHSVZeroH + bHSV.x) - .5f) - 1.f);
 	
 	// emphasize the Yellow, Cyan, Magenta
-	simd_float3 huePow = pow(hueRGB,1.5f);
+	simd_float3 huePow = pow(hueRGB,2.0f);
 	hueRGB = 1.0f - huePow;
 	
 	// balance the value so that the yellow and magenta fade to black at a similar rate
@@ -59,7 +94,7 @@ simd_float3 bHSVToRGB(float h, float s, float v){
  * to indicate musical pitches.
  */
 void freqToRGBColour(float freqHz, float *rgb){
-	float hue = log2f(freqHz);
+	float hue = simd_fract(log2f(freqHz));
 	simd_float3 rgb3 = bHSVToRGB(hue,1.0,0.5);
 	rgb[0] = rgb3.x;
 	rgb[1] = rgb3.y;
