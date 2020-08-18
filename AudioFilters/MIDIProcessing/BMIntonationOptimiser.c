39faf107d6a0bbc0d8563dece983a0b64df557d4
Unknown <sirhans@gmail.com>
2020-08-18 11:58:10 +0700

    add intontation optimiser (empty files so far)

---
+++ AudioFilters/MIDIProcessing/BMIntonationOptimiser.c
@@ -0,0 +1,9 @@
+//
+//  BMIntonationOptimiser.c
+//  AudioFiltersXcodeProject
+//
+//  Created by hans anderson on 6/12/20.
+//  Copyright © 2020 BlueMangoo. All rights reserved.
+//
+
+#include "BMIntonationOptimiser.h"
---
+++ AudioFilters/MIDIProcessing/BMIntonationOptimiser.h
@@ -0,0 +1,14 @@
+//
+//  BMIntonationOptimiser.h
+//  AudioFiltersXcodeProject
+//
+//  Created by hans anderson on 6/12/20.
+//  Copyright © 2020 BlueMangoo. All rights reserved.
+//
+
+#ifndef BMIntonationOptimiser_h
+#define BMIntonationOptimiser_h
+
+#include <stdio.h>
+
+#endif /* BMIntonationOptimiser_h */
