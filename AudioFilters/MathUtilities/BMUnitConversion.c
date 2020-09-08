b62aacc75cc1033b3ac118ddd7fedcac274e95a9
Unknown <sirhans@gmail.com>
2020-09-09 00:02:55 +0700

    add file for unit conversions

---
+++ AudioFilters/MathUtilities/BMUnitConversion.c
@@ -0,0 +1,9 @@
+//
+//  BMUnitConversion.c
+//  OscilloScope
+//
+//  Created by hans anderson on 9/8/20.
+//  Copyright © 2020 Anh Nguyen Duc. All rights reserved.
+//
+
+#include "BMUnitConversion.h"
---
+++ AudioFilters/MathUtilities/BMUnitConversion.h
@@ -0,0 +1,14 @@
+//
+//  BMUnitConversion.h
+//  OscilloScope
+//
+//  Created by hans anderson on 9/8/20.
+//  Copyright © 2020 Anh Nguyen Duc. All rights reserved.
+//
+
+#ifndef BMUnitConversion_h
+#define BMUnitConversion_h
+
+#include <stdio.h>
+
+#endif /* BMUnitConversion_h */
