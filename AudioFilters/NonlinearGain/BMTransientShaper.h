b721305a7856dd10760d3f14d3aed2f8c62d9976
Nguyen Minh Tien <tiennm642@gmail.com>
2021-12-02 14:37:39 +0700

    update settings

--- AudioFilters/NonlinearGain/BMTransientShaper.h
+++ AudioFilters/NonlinearGain/BMTransientShaper.h
@@ -94,7 +94,7 @@ typedef struct BMTransientShaper {
     float *b1L, *b2L, *b1R, *b2R;
     float* inputBuffer;
     bool isStereo;
-    
+    bool isInit;
 } BMTransientShaper;
 
 void BMTransientShaper_init(BMTransientShaper *This, bool isStereo, float sampleRate,bool isTesting);
