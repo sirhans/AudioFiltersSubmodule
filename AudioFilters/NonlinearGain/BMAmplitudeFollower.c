9dd3871bbf315a360b8cd6994923d668d14256f6
Nguyen Minh Tien <tiennm642@gmail.com>
2021-12-16 11:17:30 +0700

    update

---
+++ AudioFilters/NonlinearGain/BMAmplitudeFollower.c
@@ -0,0 +1,8 @@
+//
+//  BMAmplitudeFollower.c
+//  BMAUAmpVelocityFilter
+//
+//  Created by Nguyen Minh Tien on 16/12/2021.
+//
+
+#include "BMAmplitudeFollower.h"
---
+++ AudioFilters/NonlinearGain/BMAmplitudeFollower.h
@@ -0,0 +1,18 @@
+//
+//  BMAmplitudeFollower.h
+//  BMAUAmpVelocityFilter
+//
+//  Created by Nguyen Minh Tien on 16/12/2021.
+//
+
+#ifndef BMAmplitudeFollower_h
+#define BMAmplitudeFollower_h
+
+#include <stdio.h>
+#include "Constants.h"
+
+typedef struct BMAmplitudeFollower {
+   
+} BMAmplitudeFollower;
+
+#endif /* BMAmplitudeFollower_h */
