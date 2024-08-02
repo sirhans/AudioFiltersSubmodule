180c343d664e58c507dd90ecf2f7fa959bf9f33d
Hans Anderon (SUTD) <sirhans@gmail.com>
2024-08-02 14:24:46 -0500

    update comment for intonation optimiser

--- AudioFilters/MIDIProcessing/BMIntonationOptimiser.h
+++ AudioFilters/MIDIProcessing/BMIntonationOptimiser.h
@@ -12,6 +12,13 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+/*!
+ *BMIntonationOptimiser_processNoteList
+ *
+ * @param MIDINoteList is an array of ints where each number is a MIDI note
+ * @param pitchCorrectionList output will be written here. the nth element of this list is the correction for the nth MIDI note. A correction of 1.0 means bend up 1 full MIDI note. Actual correction will always be less than 0.16 MIDI notes.
+ * @param numNotes length of MIDINoteList and pitchCorrectionList
+ */
 int BMIntonationOptimiser_processNoteList(const int *MIDINoteList, float *pitchCorrectionList, size_t numNotes);
 
 
